fb6c0f58c9070118922b45df3de7080dd1c2ea7a
“Spencer <srye1140@gmail.com>
2023-03-17 16:35:11 -0700

    Attempt to add new-post form

--- public/js/newPost.js
+++ public/js/newPost.js
@@ -1,8 +1,8 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-  const title = document.querySelector('#post-title').value.trim();
-  const content = document.querySelector('#post-content').value.trim();
+  const title = document.querySelector('input[name="post-title"]').value.trim();
+  const content = document.querySelector('textarea[name="post-content"]').value.trim();
 
   const response = await fetch('/api/posts', {
     method: 'POST',
